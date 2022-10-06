9af2bae5cbbe80e661734abe6e354634185ce95a
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-06 16:26:41 +0600

    defined a view method to fetch all books

--- book_api/views.py
+++ book_api/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from book_api.models import Book
+
+
+def book_list(request):
+    books = list(Book.objects.all().values())
+    
+    return JsonResponse({
+        'books': books
+    })
